9ba12781a2a1497840df3d49d42886f8a325355a
Siwakorn Sittilerdpaisal <s.sittilerdpaisal@gmail.com>
2025-02-11 13:11:01 +0700

    feat(pdf): adding how to buy N3 mvp-2-2.pdf

---
+++ export-images.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next-export-optimize-images').Config}
+ */
+const config = {
+  filenameGenerator: ({ name, width, extension }) => {
+    const unixTimestamp = Math.floor(Date.now() / 1000);
+
+    return `${name}-${width}-${unixTimestamp}.${extension}`;
+  },
+};
+
+module.exports = config;
