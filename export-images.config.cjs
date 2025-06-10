6840868b797fefd4c8d37d8b46eff463944a3f58
Boss <s.sittilerdpaisal@gmail.com>
2025-06-10 22:31:13 +0700

    fix(): pipeline

--- export-images.config.cjs
+++ export-images.config.cjs
@@ -2,13 +2,7 @@
  * @type {import('next-export-optimize-images').Config}
  */
 const config = {
-  // filenameGenerator: ({ name, width, extension }) => {
-  //   const unixTimestamp = Math.floor(Date.now() / 1000);
-
-  //   return `${name}-${width}-${unixTimestamp}.${extension}`;
-  // },
   generateFormats: ['avif', 'webp'],
-
 };
 
 module.exports = config;
